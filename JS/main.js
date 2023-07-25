9b274b64d876b7b9162fc2521a9e506f6ff38dec
Ruben <villamorlage@gmail.com>
2023-07-25 12:54:54 +0200

    changes

--- JS/main.js
+++ JS/main.js
@@ -2,37 +2,40 @@
 
 const btnStartNode = document.querySelector("#start-btn");
 const gameBoxNode = document.querySelector("#game-box");
-// const alonso = document.querySelector("#alonso");
+const alonso = document.querySelector('#alonso')
 // const splashScreenNode = document.querySelector("#splash-screen");
 // const winGameNode = document.querySelector("#wingame-screen");
 
+
+
 // Management Functions
 
-function startGame() {
-  btnStartNode.style.display = "none";
-  // gameBoxNode.style.animation = "roadMovement infinite 20s linear"
+function startGame () {
 
-  // que Inicie el juego
+    btnStartNode.style.display = 'none'
+    gameBoxNode.style.animation = "roadMovement infinite 20s linear"
 
-  let gameObjeto = new Game();
-  console.log(gameObjeto);
-  gameObjeto.gameLoop();
+    // que Inicie el juego
+
+    let top = 15
+    let right = 0
+    window.addEventListener('keydown', function (tecla) {
+        console.log(tecla.key)
+        if (tecla.key == 'ArrowUp') {
+            top -= 3
+            alonso.style.top = `${top}vh`;
+        }
+        if (tecla.key == 'ArrowRight') {
+            right += 3
+            alonso.style.left = `${right}px`;
+        }
+    })
+
+    let gameObjeto = new Game();
+    console.log(gameObjeto)
+    gameObjeto.gameLoop()
 }
 
 // Event Listeners
 
-btnStartNode.addEventListener("click", startGame);
-
-window.addEventListener("keydown", function (tecla) {
-  let top = 50;
-  let right = 1200;
-  console.log(tecla.key);
-  if (tecla.key == "ArrowUp") {
-    top -= 3;
-    gameObjeto.alonso.style.top = `${top}px`;
-  }
-  if (tecla.key == "ArrowRight") {
-    right += 3;
-    gameObjeto.alonso.style.left = `${right}px`;
-  }
-});
+btnStartNode.addEventListener("click", startGame)
